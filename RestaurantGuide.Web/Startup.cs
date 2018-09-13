53fccb17acefce4147d624463265a2ea54170eec
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-14 00:27:30 +0300

    Add Web project

---
+++ RestaurantGuide.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantGuide.Web.Startup))]
+namespace RestaurantGuide.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
