901a3dea1270abc4ce5e4e1f67dccd9767e0a793
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-15 00:44:16 +0300

    Add about model

---
+++ RestaurantGuide.Web/Models/AboutModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantGuide.Web.Models
+{
+    public class AboutModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+    }
+}
