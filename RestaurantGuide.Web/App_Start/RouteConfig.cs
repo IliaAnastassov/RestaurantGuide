183f1766a9f87b3f04f7c5b8c6f72ec8258a2944
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-15 18:49:23 +0300

    Add routes / refactor

--- RestaurantGuide.Web/App_Start/RouteConfig.cs
+++ RestaurantGuide.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RestaurantGuide.Web
@@ -12,6 +8,12 @@ namespace RestaurantGuide.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Cuisine",
+                url: "cuisine/{name}",
+                defaults: new { controller = "Cuisine", action = "Search", name = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
                 name: "Default",
