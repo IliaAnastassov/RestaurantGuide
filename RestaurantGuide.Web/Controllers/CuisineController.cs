3ac96a666f43c3c7d8dcd91328529ca1a2087d2c
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-15 18:49:11 +0300

    Add cuisine controller

---
+++ RestaurantGuide.Web/Controllers/CuisineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantGuide.Web.Controllers
+{
+    public class CuisineController : Controller
+    {
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+            return Content(message);
+        }
+    }
+}
