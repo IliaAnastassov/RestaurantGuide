aa21d74f0445dd59565323dcf5cd7ed9138867a9
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-17 00:02:03 +0300

    Add custom action filter attribute

---
+++ RestaurantGuide.Web/Filters/LogAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace RestaurantGuide.Web.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
