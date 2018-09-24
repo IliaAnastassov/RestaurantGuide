f42171ba4d8bc3b89fbf9af7d2457141fcf1035d
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-24 16:43:26 +0300

    Add data acces layer

---
+++ RestaurantGuide.DataAccess/RestaurantGuideContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantGuide.Entities;
+
+namespace RestaurantGuide.DataAccess
+{
+    class RestaurantGuideContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> RestaurantReviews { get; set; }
+    }
+}
