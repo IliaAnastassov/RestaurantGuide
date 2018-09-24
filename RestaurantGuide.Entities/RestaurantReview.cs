107e4cdb28b1f11029fd972413d55270481e3d53
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-24 16:42:40 +0300

    Add entities project

---
+++ RestaurantGuide.Entities/RestaurantReview.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantGuide.Entities
+{
+    public class RestaurantReview
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Body { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
