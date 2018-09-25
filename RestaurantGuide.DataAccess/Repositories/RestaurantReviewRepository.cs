42383fb0cca85740f9687ef54f7ca6a5c6ed2d30
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-25 20:56:07 +0300

    Add repository for each entity

--- RestaurantGuide.DataAccess/Repositories/RestaurantReviewRepository.cs
+++ RestaurantGuide.DataAccess/Repositories/RestaurantReviewRepository.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestaurantGuide.Entities;
 
 namespace RestaurantGuide.DataAccess.Repositories
 {
-    public interface IRepository<T>
-        where T : new()
+    public class RestaurantReviewRepository : IRepository<RestaurantReview>
     {
     }
 }
---
+++ RestaurantGuide.DataAccess/RestaurantGuideDb.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using RestaurantGuide.Entities;
+
+namespace RestaurantGuide.DataAccess
+{
+    class RestaurantGuideDb : DbContext
+    {
+        public RestaurantGuideDb() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> RestaurantReviews { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
