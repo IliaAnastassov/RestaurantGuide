e71a03e7c1b716309a8566991e3a291191d68000
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-25 21:36:31 +0300

    Refactor usings for consistency. Add more seed data.

--- RestaurantGuide.DataAccess/Migrations/Configuration.cs
+++ RestaurantGuide.DataAccess/Migrations/Configuration.cs
@@ -1,11 +1,9 @@
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using RestaurantGuide.Entities;
+
 namespace RestaurantGuide.DataAccess.Migrations
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-    using RestaurantGuide.Entities;
 
     internal sealed class Configuration : DbMigrationsConfiguration<RestaurantGuideDb>
     {
@@ -20,7 +18,17 @@ namespace RestaurantGuide.DataAccess.Migrations
                 new Restaurant { Name = "Joe's Pizza Place", City = "New York", Country = "USA" },
                 new Restaurant { Name = "City Wok", City = "London", Country = "UK" },
                 new Restaurant { Name = "La Belle Vue", City = "Paris", Country = "France" },
-                new Restaurant { Name = "Spaggo", City = "Rome", Country = "Italy" },
+                new Restaurant
+                {
+                    Name = "Spaggo",
+                    City = "Rome",
+                    Country = "Italy",
+                    Reviews = new List<RestaurantReview>
+                    {
+                        new RestaurantReview { Rating = 9, Body = "Genuine Italian food - a must visit for all italian food lovers." },
+                        new RestaurantReview { Rating = 10, Body = "Food, wine and service are impecable!" }
+                    }
+                },
                 new Restaurant
                 {
                     Name = "Gute Schteltze",
