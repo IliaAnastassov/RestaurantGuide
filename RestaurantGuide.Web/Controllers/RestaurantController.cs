6450a664548125512487f0b8f484dea014a31326
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-27 23:58:48 +0300

    Add Restaurant controller and corresponding views (scaffold)

---
+++ RestaurantGuide.Web/Controllers/RestaurantController.cs
@@ -0,0 +1,123 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using RestaurantGuide.DataAccess.Repositories.Interfaces;
+using RestaurantGuide.Entities;
+
+namespace RestaurantGuide.Web.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private IRestaurantRepository _repository;
+
+        public RestaurantController(IRestaurantRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            var restaurants = _repository.GetAll();
+
+            return View(restaurants);
+        }
+
+        // GET: Restaurant/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _repository.Get((int)id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,City,Country")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Restaurants.Add(restaurant);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
+        // GET: Restaurant/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _repository.Get((int)id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,City,Country")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(restaurant).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
+        // GET: Restaurant/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _repository.Get((int)id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var restaurant = _repository.Get(id);
+            //db.Restaurants.Remove(restaurant);
+            //db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
