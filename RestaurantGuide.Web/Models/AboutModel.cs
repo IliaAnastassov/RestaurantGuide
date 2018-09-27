e19e827f5436c2b65225fabeb2da9f62ee18bd93
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-27 18:42:39 +0300

    Remove unused model, add new veiew model

--- RestaurantGuide.Web/Models/AboutModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantGuide.Web.Models
-{
-    public class AboutModel
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Location { get; set; }
-    }
-}
---
+++ RestaurantGuide.Web/Models/RestaurantListViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantGuide.Web.Models
+{
+    public class RestaurantListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int CountOfReviews { get; set; }
+        public double? Rating { get; set; }
+    }
+}
