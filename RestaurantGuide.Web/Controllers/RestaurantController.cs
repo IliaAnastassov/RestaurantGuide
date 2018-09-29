013eca49c1720fda41af990a6d3338336397b6c7
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-29 12:25:26 +0300

    Remove details view

--- RestaurantGuide.Web/Controllers/RestaurantController.cs
+++ RestaurantGuide.Web/Controllers/RestaurantController.cs
@@ -22,23 +22,6 @@ namespace RestaurantGuide.Web.Controllers
             return View(restaurants);
         }
 
-        // GET: Restaurant/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Restaurant restaurant = _repository.Get((int)id);
-            if (restaurant == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(restaurant);
-        }
-
         // GET: Restaurant/Create
         public ActionResult Create()
         {
