4e34495c82e2c0c0a2881271e2799bb749b4ee80
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-30 17:02:31 +0300

    Add custom data validation

--- RestaurantGuide.Entities/RestaurantReview.cs
+++ RestaurantGuide.Entities/RestaurantReview.cs
@@ -1,16 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RestaurantGuide.Entities
 {
-    public class RestaurantReview
+    public class RestaurantReview : IValidatableObject
     {
         public int Id { get; set; }
+
         [Required, Range(1, 10)]
         public int Rating { get; set; }
+
         [Required, StringLength(maximumLength: 1024, MinimumLength = 5)]
         public string Body { get; set; }
+
         [StringLength(100)]
+        [Display(Name = "User Name"), DisplayFormat(NullDisplayText = "Anonymous")]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating <= 2 && ReviewerName.ToLower().StartsWith("eric"))
+            {
+                yield return new ValidationResult("Stop giving bad results, Eric!");
+            }
+        }
     }
 }
