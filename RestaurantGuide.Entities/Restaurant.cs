ce1a22c844efa509f07b1e2242fd06c505e13da0
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-09-30 16:17:56 +0300

    Add data annotations

--- RestaurantGuide.Entities/Restaurant.cs
+++ RestaurantGuide.Entities/Restaurant.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RestaurantGuide.Entities
 {
     public class Restaurant
     {
         public int Id { get; set; }
+        [Required, StringLength(100)]
         public string Name { get; set; }
+        [Required, StringLength(100)]
         public string City { get; set; }
+        [Required, StringLength(100)]
         public string Country { get; set; }
 
         public virtual ICollection<RestaurantReview> Reviews { get; set; }
--- RestaurantGuide.Entities/RestaurantReview.cs
+++ RestaurantGuide.Entities/RestaurantReview.cs
@@ -1,10 +1,15 @@
-﻿namespace RestaurantGuide.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantGuide.Entities
 {
     public class RestaurantReview
     {
         public int Id { get; set; }
+        [Required, Range(1, 10)]
         public int Rating { get; set; }
+        [Required, StringLength(maximumLength: 1024, MinimumLength = 5)]
         public string Body { get; set; }
+        [StringLength(100)]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
     }
