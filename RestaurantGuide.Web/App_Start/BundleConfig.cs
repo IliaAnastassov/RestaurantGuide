a485613a24e1a7c698079ed66b100ac86c7a3dbf
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-02 16:34:00 +0300

    Add Unobtrusive Ajax to get the async search to work

--- RestaurantGuide.Web/App_Start/BundleConfig.cs
+++ RestaurantGuide.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace RestaurantGuide.Web
         {
             bundles.Add(new ScriptBundle("~/bundles/restaurantGuide").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jquery.unobtrusive*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
