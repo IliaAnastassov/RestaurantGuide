8c59545e07a96817886d62fe04ae04dab16eaeb0
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-08 18:23:09 +0300

    Disable automatic migrations

--- RestaurantGuide.DataAccess/Migrations/Configuration.cs
+++ RestaurantGuide.DataAccess/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace RestaurantGuide.DataAccess.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(RestaurantGuideDb context)
